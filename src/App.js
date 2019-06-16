ee9a22c304dc3ce75af0916e970b82238297b28e
Christopher Aubuchon <aubuchcl@gmail.com>
2019-06-16 14:06:37 -0700

    making qol changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> hot reload reload reload.
+	  <br />
+	  hot reload?
         </p>
         <a
           className="App-link"
