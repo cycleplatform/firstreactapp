70a450ed98ab546f8a48a63fdb22a330cc5b5471
Christopher Aubuchon <aubuchcl@gmail.com>
2019-09-16 21:49:37 -0700

    the script will now use the updated name for the container

--- devscript.sh
+++ devscript.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t firstreactapp -f _Dockerfile.dev .
-docker run -it --mount type=bind,source="$(pwd)"/src,target=/src -p 3000:3000 hellocycledev
+docker build -t firstreactapp -f Dockerfile.dev .
+docker run -it --mount type=bind,source="$(pwd)"/src,target=/src -p 3000:3000 firstreactapp
